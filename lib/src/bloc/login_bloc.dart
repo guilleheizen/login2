b8f208fc128fd25e302ee89ec6f4168d492fdd9c
Mega <guilleheizen@gmail.com>
2021-04-21 15:00:36 -0400

    Add rx

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
-
 import 'package:login2/src/bloc/validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class LoginBloc with Validators {
-  final _emailController = StreamController<String>.broadcast();
-  final _passwordController = StreamController<String>.broadcast();
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
 // GETTERS
   Stream<String> get emailStream =>
@@ -12,9 +12,13 @@ class LoginBloc with Validators {
   Stream<String> get passwordStream =>
       _passwordController.stream.transform(validarPassword);
 
+  Stream<bool> get fromValidStream => Rx.combineLatest2(emailStream, passwordStream, (e,p) => true);
+
+
 // SETTERS
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
+
 
   dispose() {
     _emailController?.close();
